9d5f4b1946e471ed4a861bea84f15a34d4fa3d8a
CHO <bsuysheep927@gmail.com>
2022-09-16 15:37:46 +0800

    test mobile scroll

--- js/public.js
+++ js/public.js
@@ -1,9 +1,13 @@
 
 $(function() {
-	document.body.addEventListener('touchstart', function(e){ 
-		// document.getElementsByTagName('body')[0]. style .height = "100vh";
-		document.getElementsByTagName('html')[0]. style. overflow = "hidden";
-	});
+	// document.body.addEventListener('touchstart', function(e){ 
+	// 	document.getElementsByTagName('body')[0]. style .height = "100vh";
+	// 	document.getElementsByTagName('html')[0]. style. overflow = "hidden";
+	// });
+	
+	// document.body.addEventListener('touchstart', function(e){ e.preventDefault(); });
+	// document.body.addEventListener('touchmove', function(e){ e.preventDefault(); }); 
+	
 	// check img is complete loading
 	function imageLoaded() {
 		counter--; 
